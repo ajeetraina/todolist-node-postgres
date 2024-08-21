307035708ae3f44c8d8b900c5b77826befc417a9
Ajeet Singh Raina <ajeetraina@gmail.com>
2024-08-21 16:32:29 +0530

    Pre-seeding postgres

---
+++ postgres/migration/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO test.todos (id, title, completed)
+VALUES
+  (1, 'Task 1', false),
+  (2, 'Task 2', true),
+  (3, 'Task 3', false);
---
+++ postgres/migration/schema.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS test;
+
+SET search_path TO test;
+
+CREATE TABLE IF NOT EXISTS todos (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  completed BOOLEAN DEFAULT FALSE
+);
